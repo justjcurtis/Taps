4bcc471839d851448f1d49e6f45ed1be88bc9234
justjcurtis <justjcurtis@gmail.com>
2023-07-13 17:55:21 +0100

    fix: fixed phone taps; feat: add to hud; fix: fixed styling & layout issues

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
 let w;
 let h;
+const hMod = 0.88;
 function setup() {
   w = window.innerWidth;
-  h = window.innerHeight - window.innerHeight * 0.1;
+  h = window.innerHeight * hMod;
   createCanvas(w, h, P2D);
-  createElement("button", "Download json").mousePressed(downloadJSON);
-  createElement("button", "Export taps to CSV").mousePressed(exportTapsToCSV);
 }
 
 const min = 0;
@@ -16,6 +15,8 @@ let touching = false;
 const taps = [];
 let tapStart;
 let started = false;
+let maxTapTime = 0;
+let maxBetweenTime = 0;
 
 const getHud = () => {
   const total = taps
@@ -37,7 +38,15 @@ const getHud = () => {
     ) / 1000;
   const betweenTotal = between.reduce((acc, curr) => acc + curr);
 
-  return { total, averageLength, between, averageBetween, betweenTotal };
+  return {
+    total,
+    averageLength,
+    between,
+    averageBetween,
+    betweenTotal,
+    maxTapTime,
+    maxBetweenTime,
+  };
 };
 
 const exportTapsToCSV = () => {
@@ -76,11 +85,17 @@ const handleBG = () => {
 };
 
 const handleLine = () => {
-  if (taps.length < 2) return;
+  if (!started) return;
   stroke(100, 0, 100);
   strokeWeight(2);
   noFill();
   beginShape();
+  if (taps.length < 2) {
+    vertex(0, h * hMod);
+    vertex(w, h * hMod);
+    endShape();
+    return;
+  }
   const slice = taps.slice(-10);
   const { minY, maxY } = slice.reduce(
     (acc, { tapStart, tapEnd }) => {
@@ -96,7 +111,7 @@ const handleLine = () => {
   };
   slice.forEach(({ tapStart, tapEnd }) => {
     const x = map(tapStart, minX, maxX, 0, w);
-    const y = map(tapEnd - tapStart, minY, maxY, h * 0.9, h * 0.9 - 100);
+    const y = map(tapEnd - tapStart, minY, maxY, h * hMod, h * hMod - 100);
     vertex(x, y);
   });
   endShape();
@@ -107,17 +122,53 @@ const handleText = () => {
   textSize(20);
   fill(255);
   noStroke();
-  const { total, averageLength, between, averageBetween, betweenTotal } =
-    getHud();
+  const {
+    total,
+    averageLength,
+    averageBetween,
+    betweenTotal,
+    maxBetweenTime,
+    maxTapTime,
+  } = getHud();
   text("Taps: " + taps.length, 30, 30);
   text("Average on time: " + averageLength + "s", 30, 60);
   text("Average between time: " + averageBetween, 30, 90);
 
-  text("Total on time: " + averageLength + "s", 30, 150);
-  text("Total between time: " + betweenTotal / 1000 + "s", 30, 180);
-  text("Total time: " + (total + betweenTotal) / 1000 + "s", 30, 210);
+  text("Max tap time: " + maxTapTime / 1000 + "s", 30, 150);
+  text("Max between time: " + maxBetweenTime / 1000 + "s", 30, 180);
+
+  text("Total on time: " + averageLength + "s", 30, 240);
+  text("Total between time: " + betweenTotal / 1000 + "s", 30, 270);
+  text("Total time: " + (total + betweenTotal) / 1000 + "s", 30, 300);
 };
 
+const handleLog = () => {
+  if (enableLog) {
+    textSize(10);
+    fill(255);
+    noStroke();
+    text(
+      `${logArr.slice(-20).join("\n")}`,
+      30,
+      h * hMod -
+        9 * Math.min(20, logArr.length) +
+        3.2 * (20 - Math.min(20, logArr.length))
+    );
+  }
+};
+
+const log = (...msgs) => {
+  let logMsg = msgs
+    .map((msg) => {
+      if (typeof msg !== "string") return JSON.stringify(msg);
+      return msg;
+    })
+    .join(", ");
+  logArr.push(logMsg);
+};
+
+const logArr = [];
+let enableLog = false;
 function draw() {
   handleBG();
   handleLine();
@@ -127,22 +178,31 @@ function draw() {
     text("Touch to start", w / 2 - 100, h / 2);
   }
   handleText();
+  handleLog();
 }
 
 function touchStarted() {
-  if (mouseY > h * 0.9) return;
+  if (touching) return;
+  if (mouseY > h * hMod) return;
   touching = true;
   tapStart = millis();
 }
 
 function touchEnded() {
   if (!touching) return;
+  touching = false;
   if (!started) {
     started = true;
-    touching = false;
     return;
   }
-  touching = false;
-  taps.push({ tapStart, tapEnd: millis() });
+  const tap = { tapStart, tapEnd: millis() };
+  const length = tap.tapEnd - tap.tapStart;
+  if (length < 10) return;
+  taps.push(tap);
+  log(tap, length);
+  if (length > maxTapTime) maxTapTime = length;
+  const lastTap = taps[taps.length - 2].tapEnd;
+  const between = tap.tapStart - lastTap;
+  if (between > maxBetweenTime) maxBetweenTime = between;
   tapStart = null;
 }
